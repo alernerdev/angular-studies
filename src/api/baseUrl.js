9c78a91e7a1d9bd204d841dbf734dfe228c0fd5d
Alex Lerner <alernerdev@gmail.com>
2017-12-10 17:22:59 -0500

    separated backend into a separate project deployed to Heroku. Deployed the front end to Surge.  It now works end to end running on different services

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,11 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    // mockAPI hits jsonServer which generates random data every time based on the schema we have specified
+    // the jsonServer has been configured for port 3001
+    // when using "real" data, we are hitting / which is hosted by express on my local machine
+    //return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+
+    // after I have migrated the backend to heroku, use this instead.
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://enigmatic-caverns-55214.herokuapp.com/';
 }
 
 
