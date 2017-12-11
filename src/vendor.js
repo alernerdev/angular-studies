7aa3e604a5a90bf0e4b7e4e9644347b801f47eda
Alex Lerner <alernerdev@gmail.com>
2017-12-11 07:44:05 -0500

    started off with js-dev-env and adding angular to the kit

--- src/vendor.js
+++ src/vendor.js
@@ -3,4 +3,5 @@ This is used by webpack in the production build only
 This gets built into a separate bundle
 */
 
-/* eslint-disable no-ununsed-vars */
+/* eslint-disable no-unused-vars */
+import angular from 'angular';
