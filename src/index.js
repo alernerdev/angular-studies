2627e19ba7970bac7131a4f00a00e74614055e81
Alex Lerner <alernerdev@gmail.com>
2017-12-16 18:43:26 -0500

    at this point I have angular libraries included twice in the bundles, but at least routing works.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import './index.css';
+import angular from 'angular';
+import ngRoute from 'angular-route';
 
 /* eslint-disable no-console */
 console.log("welcome from index.js");
 
+
+var app = angular.module('myStudyApp', ['ngRoute']);
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.when('/', {
+		redirectTo:'/start'
+	});
+	$routeProvider.when('start', {
+		template: "<h6> my start partial is working ? </h6>"
+		//templateUrl:'partials/HelloWorld2.html'
+	});
+	$routeProvider.when('/finish', {
+		template: "<h6> my finish partial is working ? </h6>"
+		//templateUrl:'partials/HelloWorld2.html'
+	});
+	$routeProvider.otherwise({
+		template: "<h6> routing otherwise </h6>"
+	});
+
+	//$locationProvider.html5Mode(true);
+}]);
+
+app.controller('myController', ['$scope', function($scope) {
+
+}]);
 
 
 
